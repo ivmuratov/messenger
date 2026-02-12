7e7307ce3f5786ff84a1cd940cfd376b1b817583
muratov <vania.muratoff@yandex.ru>
2026-02-12 14:03:26 +0700

    ui - add Sidebar directories

--- packages/ui/src/index.native.ts
+++ packages/ui/src/index.native.ts
@@ -1,2 +1,3 @@
 export * from "./atoms/index.native";
+export * from "./organisms/index.native";
 export * from "./ThemeProvider/index.native";
---
+++ packages/ui/src/organisms/Sidebar/mobile/Sidebar.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export const Sidebar = () => {
+  return <View>Sidebar</View>;
+};
