77d6c99a4f05fe594097dab9cfdd6ffefffc7c9a
muratov <vania.muratoff@yandex.ru>
2026-02-27 22:48:07 +0700

    ui - fix NativeStyles

--- packages/ui/src/libs/mobile/types.ts
+++ packages/ui/src/libs/mobile/types.ts
@@ -1,3 +1,3 @@
 import type { ImageStyle, TextStyle, ViewStyle } from "react-native";
 
-export type NativeStyle = ViewStyle | TextStyle | ImageStyle;
+export type NativeStyle = ViewStyle & TextStyle & ImageStyle;
